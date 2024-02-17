3a83e77bdf6b3a7d02aa05d1e6b20c64aa8b8207
okkyarrohman <okkyzee@gmail.com>
2024-02-17 17:29:10 +0700

    models clear

--- app/Models/Proyek.php
+++ app/Models/Proyek.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Proyek extends Model
 {
     use HasFactory;
+
+    protected $table = 'proyeks';
+
+    protected $fillable = [
+        'tugas',
+        'tenggat',
+        'step1',
+        'deskripsi1',
+        'step2',
+        'deskripsi2',
+        'step3',
+        'deskripsi3',
+        'step4',
+        'deskripsi4',
+    ];
+
+    public function proyekResult()
+    {
+        return $this->hasMany(ProyekResult::class);
+    }
 }
--- app/Models/Pustaka.php
+++ app/Models/Pustaka.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Pustaka extends Model
 {
     use HasFactory;
+    protected $table = 'pustakas';
+
+    protected $fillable = [
+        'judul',
+        'sumber',
+        'cover',
+        'file'
+    ];
 }
